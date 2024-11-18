6a33de20c279f5bd06a3f27d21886adf0c86b853
VinceCat08 <bandcatworld@gmail.com>
2024-11-18 11:33:22 +0300

    module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,12 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
+    str_number = str_number.replace('0', '')
     for i in range(len(str_number) > 1):
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(4020)
 print(result)
 
 
